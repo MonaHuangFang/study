d76fa8933cefb9ce96c9c4f73e753c21ce7a4544
huangf <1014759092@qq.com>
2020-04-14 18:34:11 +0800

    添加顺序栈

--- 数据结构/005-Stack/005-Stack/main.c
+++ 数据结构/005-Stack/005-Stack/main.c
@@ -24,6 +24,72 @@ typedef struct {
     int top;
 }OrderStack;
 
+//构建空栈
+Status initStack(OrderStack *s){
+    s->top = -1;
+    return OK;
+}
+
+//清空栈
+Status clearStack(OrderStack *s){
+    s->top = -1;
+    return OK;
+}
+//判断是否空栈
+Status stackIsEmpty(OrderStack s){
+    if (s.top == -1) {
+        return TRUE;
+    }else{
+        return FALSE;
+    }
+}
+
+//返回栈的长度
+int stackCount(OrderStack s){
+    return s.top + 1;
+}
+
+//获取栈顶
+Status getTop(OrderStack s, SElemType *e){
+    if (s.top == -1) {
+        return ERROR;
+    }else{
+        *e = s.data[s.top];
+        return OK;
+    }
+}
+
+//插入元素
+Status push(OrderStack *s, SElemType e){
+    //栈满
+    if (s->top == MAXSIZE - 1) {
+        return ERROR;
+    }
+    
+    s->top ++ ;
+    s->data[s->top] = e;
+    
+    return OK;
+}
+
+//删除元素
+Status pop(OrderStack *s, SElemType *e){
+    if (s->top == -1) {
+        return ERROR;
+    }
+    *e = s->data[s->top];
+    s->top--;
+    return OK;
+}
+
+//遍历
+void display(OrderStack s){
+    printf("栈内元素：");
+    for (int i = 0; i<=s.top; i++) {
+        printf("%3d",s.data[i]);
+    }
+    printf("\n");
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
