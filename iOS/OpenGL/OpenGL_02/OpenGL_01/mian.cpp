a5e1f52cd6fa0c57dd3762afa3e1f2a865a2842f
MonaHuangFang <1014759092@qq.com>
2020-07-14 14:48:06 +0800

    add demo

--- iOS/OpenGL/OpenGL_02/OpenGL_01/mian.cpp
+++ iOS/OpenGL/OpenGL_02/OpenGL_01/mian.cpp
@@ -17,7 +17,7 @@ GLBatch triangleBatch;
 GLShaderManager shaderManager;
 
 //设置正方形参赛
-GLfloat blockSize = 0.2f;
+GLfloat blockSize = 0.1f;
 
 GLfloat vVerts[] = {
    -blockSize,-blockSize,0.0f,
@@ -25,6 +25,9 @@ GLfloat vVerts[] = {
     blockSize,blockSize,0.0f,
     -blockSize,blockSize,0.0f
 };
+
+GLfloat xPos = 0.0;
+GLfloat yPos = 0.0;
 
 void ChangeSize(int w, int h){
     //改变窗口大小
@@ -56,58 +59,31 @@ void SetupRC(){
 
 void SpecialKeys(int key, int x, int y){
     GLfloat stepSize = 0.025f;
-    GLfloat blockX = vVerts[0];
-    GLfloat blockY = vVerts[10];
-    
-    printf("v[0] = %f\n",blockX);
-    printf("v[10] = %f\n",blockY);
+
     
     if(key == GLUT_KEY_UP){
-        blockY += stepSize;
+        yPos += stepSize;
     }else if (key == GLUT_KEY_DOWN){
-        blockY -= stepSize;
+        yPos -= stepSize;
     }else if (key == GLUT_KEY_LEFT){
-        blockX -= stepSize;
+        xPos -= stepSize;
     }else if (key == GLUT_KEY_RIGHT){
-        blockX += stepSize;
+        xPos += stepSize;
     }
     
-    if (blockX < -1.0f) {
-        blockX = -1.0f;
+    if(xPos < -1.0 + blockSize){
+        x = -0.1 + blockSize;
+    }
+    if(xPos > 1.0 - blockSize){
+        xPos = 1.0 - blockSize;
+    }
+    if(yPos < -1.0 + blockSize){
+        yPos = -1.0 + blockSize;
+    }
+    if (yPos > 1.0 - blockSize) {
+        yPos = 1.0 + blockSize;
     }
     
-    if (blockX > (1.0 - blockSize * 2)) {
-        blockX = 1.0f - blockSize * 2;
-    }
-    
-    if (blockY < -1.0f + blockSize * 2 ) {
-        blockY = -1.0f + blockSize * 2;
-    }
-    
-    if (blockY > 1.0f) {
-        blockY = 1.0f;
-    }
-    
-    printf("blockX = %f\n",blockX);
-    printf("blockY = %f\n",blockY);
-    
-    vVerts[0] = blockX;
-    vVerts[1] = blockY - blockSize*2;
-    printf("(%f,%f)\n",vVerts[0],vVerts[1]);
-    
-    vVerts[3] = blockX + blockSize*2;
-    vVerts[4] = blockY - blockSize*2;
-    printf("(%f,%f)\n",vVerts[3],vVerts[4]);
-    
-    vVerts[6] = blockX + blockSize*2;
-    vVerts[7] = blockY;
-    printf("(%f,%f)\n",vVerts[6],vVerts[7]);
-    
-    vVerts[9] = blockX;
-    vVerts[10] = blockY;
-    printf("(%f,%f)\n",vVerts[9],vVerts[10]);
-    
-    triangleBatch.CopyVertexData3f(vVerts);
     glutPostRedisplay();
 }
 
@@ -115,12 +91,16 @@ void SpecialKeys(int key, int x, int y){
 void RenderScene(){
     //清除一个或一组特定的缓冲区
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    
-    //设置一组浮点数来表示红色
     GLfloat vRed[] = {0.0f,1.0f,0.0f,1.0f};
     
-    //传递到存储着色器，即GLT_SHADER_IDENTITY着色器，这个着色器只是使用指定颜色以默认笛卡尔坐标在屏幕上渲染几何图形
-    shaderManager.UseStockShader(GLT_SHADER_IDENTITY,vRed);
+    //矩阵
+    M3DMatrix44f mTransf;
+    
+    //平移矩阵
+    m3dTranslationMatrix44(mTransf, xPos, yPos, 0.0);
+    
+    //平面着色器
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, mTransf, vRed);
     
     //提交着色器
     triangleBatch.Draw();
